5582144eb8d822df63b8e2c57802b0bfd2726be4
Håkon Wardeberg <hakonwar@gmail.com>
2020-11-04 14:42:23 +0100

    Delete old renderer and clean up comments

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import torch
-import pytorch3d
 from pytorch3d.io import load_ply, load_objs_as_meshes, load_obj
-from pytorch3d.structures import Meshes
 #mport matplotlib.pyplot as plt
+import model
+from tqdm import tqdm
+
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
     torch.cuda.set_device(device)
 else:
     device = torch.device("cpu")
-    #raise Exception("Missing cuda")
     print("MISSING CUDA")
-#device = torch.device("cuda:0")
+    raise Exception("Missing cuda")
+
 print(torch.cuda.is_available())
+print(device.type)
 
-
-#from pytorch3d.renderer import TexturesVertex, OpenGLPerspectiveCameras, PointLights, SoftPhongShader, TexturesUV
+# from pytorch3d.renderer import TexturesVertex, OpenGLPerspectiveCameras, PointLights, SoftPhongShader, TexturesUV
 
 #   _______ ______          _____   ____ _______
 #  |__   __|  ____|   /\   |  __ \ / __ \__   __|
@@ -25,15 +26,10 @@ print(torch.cuda.is_available())
 #     |_|  |______/_/    \_\_|     \____/  |_|
 #
 #
-verts, faces = load_ply("teapot.ply")  # (V,3) tensor, (F,3) tensor
-#model_textures = TexturesVertex(verts_features=torch.ones_like(verts, device=device)[None])
-mesh = Meshes(verts=[verts], faces=[faces])#, textures=model_textures)
+# verts, faces = load_ply("teapot.ply")  # (V,3) tensor, (F,3) tensor
+# model_textures = TexturesVertex(verts_features=torch.ones_like(verts, device=device)[None])
+# mesh = Meshes(verts=[verts], faces=[faces])#, textures=model_textures)
 
-
-#from PIL import Image
-#import numpy as np
-#im = Image.open("001.bmp")
-#p = np.array(im)
 
 #   ______            __
 #  |  ____|          /_ |
@@ -42,74 +38,30 @@ mesh = Meshes(verts=[verts], faces=[faces])#, textures=model_textures)
 #  | | | (_| | (_|  __/ |
 #  |_|  \__,_|\___\___|_|
 #
-verts, faces, aux = load_obj("001.obj")
-#model_textures = TexturesVertex(verts_features=torch.ones_like(verts, device=device)[None])
-mesh_face = Meshes(verts=[verts], faces=[faces.verts_idx])#, textures=model_textures)
+# verts, faces, aux = load_obj("001.obj")
+# model_textures = TexturesVertex(verts_features=torch.ones_like(verts, device=device)[None])
+# mesh_face = Meshes(verts=[verts], faces=[faces.verts_idx])#, textures=model_textures)
 
 
-
-
-# Trash, ikke prøv deg lol
-#model_textures_uv = TexturesUV(maps = p, faces_uvs=faces.textures_idx, verts_uvs=aux.verts_uvs)
-#mesh = load_objs_as_meshes(["001.obj"], device=device)
-#texture_image=mesh.textures.maps_padded()
-
-
-# # Imports
-# from pytorch3d.renderer import (
-#     FoVPerspectiveCameras, look_at_view_transform,
-#     RasterizationSettings, BlendParams,
-#     MeshRenderer, MeshRasterizer, HardPhongShader, SoftSilhouetteShader, Materials, TexturesVertex
-# )
-# R, T = look_at_view_transform(2.7, 10, 20)
-# cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
-#
-# raster_settings = RasterizationSettings(
-#     image_size=512,
-#     blur_radius=0.0,
-#     faces_per_pixel=1,
-# )
-#
-# #R, T = look_at_view_transform(8.0, -80, 10)
-# R, T = look_at_view_transform(80, 80, 80)
-# cameras = OpenGLPerspectiveCameras(R=R, T=T, device=device)
-# raster_settings = RasterizationSettings(image_size=512)
-# lights = PointLights(location=torch.tensor([200.0, -20, 20], device=device)[None],device=device)
-#
-# renderer = MeshRenderer(
-#     rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
-#     shader=SoftPhongShader(device=device, cameras=cameras, lights=lights)
-# )
-
-
-#images = renderer(mesh)
-#plt.figure(figsize=(10, 10))
-#plt.imshow(images[0, ..., :3].cpu().numpy())
-#plt.grid("off")
-#plt.axis("off")
-#plt.show()
-
-
-import torch
-import model
-
-print(torch.cuda.is_available())
-print(device.type)
 
 #
 # Meshes 1-3
 #
 mesh_multiple = load_objs_as_meshes(["001.obj", "002.obj", "003.obj"], device=device)
 
-model = model.TestNet(device)
-model = model.to(device)  #  a must
 
-#criterion = torch.nn.MSELoss(reduction='sum')
-#criterion = torch.nn.CrossEntropyLoss()
+# Model
+model = model.TestNet(device)
+model = model.to(device)  # a must
+
+
+# Loss & optimizer
+# criterion = torch.nn.MSELoss(reduction='sum')
+# criterion = torch.nn.CrossEntropyLoss()
 criterion = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
-#mesh = mesh.cuda()
+# Mess debug info
 mesh = mesh_multiple.to(device)
 print("verts to mesh idx:", mesh.verts_packed_to_mesh_idx())
 print("verts to mesh idx:", mesh.verts_packed_to_mesh_idx().shape)
@@ -118,30 +70,28 @@ print("Edge to mesh idx:", mesh.edges_packed_to_mesh_idx().shape)
 print("num verts per mesh:", mesh.num_verts_per_mesh())
 print("num Edges per mesh:", mesh.num_edges_per_mesh())
 
-#print("faces to mesh idx:", mesh.faces_packed_to_mesh_idx())
-#print("faces to mesh idx:", mesh.faces_packed_to_mesh_idx().shape)
-#print("faces to edges packed:", mesh.faces_packed_to_edges_packed().shape)
+# print("faces to mesh idx:", mesh.faces_packed_to_mesh_idx())
+# print("faces to mesh idx:", mesh.faces_packed_to_mesh_idx().shape)
+# print("faces to edges packed:", mesh.faces_packed_to_edges_packed().shape)
 
-#y_pred = model(mesh)
-
+# Generate label data for testing
 #y_hat = torch.tensor([0,1,2]).to(device)
 y_hat = torch.tensor([[0.0]*64, [0.0]*64, [0.0]*64,]).to(device)
 y_hat[0][0] = 1.0
 y_hat[1][1] = 1.0
 y_hat[2][2] = 1.0
 
-from tqdm import tqdm
+
 tq = tqdm(range(5000), leave=False)
 
 losses = {}
 
 for t in tq:
-    #print("t", t)
-
 
     y_pred = model(mesh)
-    #print("y_pred", y_pred.shape)
+    # print("y_pred", y_pred.shape)
     loss = criterion(y_pred, y_hat)
+
     if t % 100 == 0:
         #print(t, loss.item())
         losses[t] = loss.item()
--- model.py
+++ model.py
@@ -11,37 +11,31 @@ class TestNet(torch.nn.Module):
         super(TestNet, self).__init__()
         self.device = device
         self.debug = debug
+
         self.conv1 = pytorch3d.ops.GraphConv(input_dim=3, output_dim=5, init="normal", directed=False)
         self.conv2 = pytorch3d.ops.GraphConv(input_dim=5, output_dim=20, init="normal", directed=False)
-        #self.conv3 = pytorch3d.ops.GraphConv(input_dim=20, output_dim=50, init="normal", directed=False)
+
         self.activation = nn.LeakyReLU()
         self.flatten = nn.Flatten()
 
         siste_lag = 10
         self.fc1 = nn.Linear(20, siste_lag)
-
-        # Output? max verts * siste lag?  aka 5850 * 100?
-        self.fc2 = nn.Linear(5850*siste_lag, 64)
+        self.fc2 = nn.Linear(5850*siste_lag, 64)  # 5850, as thats the current max edges, TODO fix better way
 
         self.softmax = nn.Softmax(dim=1)
 
 
     def forward(self, input: Meshes):
+
+        # Pack the vertexs and adges
         verts = input.verts_packed().to(self.device)
-        #verts = x.verts_padded() # Gir [1, verts, 3]
+        edges = input.edges_packed().to(self.device)
 
         if self.debug: print("vertes", verts.shape)
-        edges = input.edges_packed().to(self.device)
         if self.debug: print("edges", edges.shape)
 
-        #print(11)
-
-
-        #x = self.model((verts, edges))
         x = self.conv1(verts, edges)
         x = self.activation(x)
-
-        #print(1)
 
         x = self.conv2(x, edges)
         x = self.activation(x)
