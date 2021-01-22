389f483006a54a9269f3e01ce2af945c338b9245
Håkon Wardeberg <hakonwar@gmail.com>
2021-01-22 04:18:33 +0100

    Implement network from prelim report to validate code

--- model/network.py
+++ model/network.py
@@ -1,5 +1,5 @@
 import torch
-from torch_geometric.nn import TopKPooling, GCNConv, TopKPooling
+from torch_geometric.nn import TopKPooling, GCNConv
 from torch import nn
 
 torch.manual_seed(1)
@@ -48,5 +48,42 @@ class TestNet(torch.nn.Module):
         x = self.fc1(x)
         #x = self.activation(x)
         #x = self.fc2(x)
+
+        return x
+
+
+# The model described in the reliminary report
+# DO NOT USE, as it has a lot of limitations
+# Mostly as a test to see if the current re-written code gets the same result
+class PrelimNet(torch.nn.Module):
+    def __init__(self):
+        super(PrelimNet, self).__init__()
+
+        self.activation = nn.LeakyReLU()
+
+        self.conv1 = GCNConv(in_channels=3, out_channels=5)
+        self.conv2 = GCNConv(in_channels=5, out_channels=20)
+
+        self.flatten = nn.Flatten(start_dim=0)  # Special start dim as it is not yet batched
+        self.fc1 = nn.Linear(20, 10)
+        self.fc2 = nn.Linear(5850*10, 100)
+
+    def forward(self, data):
+        pos, edge_index = data.pos, data.edge_index
+
+        x = self.conv1(pos, edge_index)
+        x = self.activation(x)
+
+        x = self.conv2(x, edge_index)
+        x = self.activation(x)
+
+        x = self.fc1(x)
+        x = self.activation(x)
+
+        # Flatten type
+        x = x.transpose(0, 1)  # transpose to regular structure of [dims, nodes]
+        x = self.flatten(x)
+        x = self.fc2(x)
+        x = self.activation(x)
 
         return x
