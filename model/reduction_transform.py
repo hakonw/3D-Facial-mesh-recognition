f74f9667fdbcf61826581ea409c98720c00936bd
Håkon Wardeberg <hakonwar@gmail.com>
2021-04-15 02:19:10 +0200

    Modify the triangle simplification

--- model/reduction_transform.py
+++ model/reduction_transform.py
@@ -1,24 +1,34 @@
 
 import torch_geometric.utils
 
+def simplify_trimesh(trimesh, target, undershootmargin=0):
+    for _ in range(20):
+        vertices = trimesh.vertices.shape[0]
+        faces = trimesh.faces.shape[0]
+
+        if (target-undershootmargin) <= vertices <= target:
+            return trimesh
+        if vertices < target - undershootmargin:
+            raise RuntimeError(f"Optimized too much. TODO fix {trimesh}")
+
+        must_remove_vertices = vertices - target
+        must_remove_faces = must_remove_vertices  # Possibly add a factor here
+        trimesh = trimesh.simplify_quadratic_decimation(faces - must_remove_faces)
+    raise RuntimeError(
+        f"Was not able to optimize within 20 generations. Stopping: {trimesh}")
+
 class SimplifyQuadraticDecimationBruteForce(object):
-    def __init__(self, vertices): 
+    def __init__(self, vertices, undershootmargin=0):
         self.verticies = vertices 
+        self.undershootmargin = undershootmargin
 
     def __call__(self, data):
         trimesh = torch_geometric.utils.to_trimesh(data)
-        for i in range(20):
-                vertices = trimesh.vertices.shape[0]
-                faces = trimesh.faces.shape[0]
+        trimesh = simplify_trimesh(trimesh, self.verticiesm, self.undershootmargin)
 
-                if vertices == self.verticies:
-                    new_data = torch_geometric.utils.from_trimesh(trimesh)
-                    data.face = new_data.face
-                    data.pos = new_data.pos
-                    return data  # Dont mess with any other properties
-                if vertices < self.verticies:
-                    raise AssertionError(f"Optimized too much. TODO fix {trimesh}")
-
-                must_remove_vertices = vertices - self.verticies
-                must_remove_faces = must_remove_vertices  # Possibly add a factor here
-                trimesh = trimesh.simplify_quadratic_decimation(faces - must_remove_faces)
+        # Reconstruct and overwrite data
+        # Dont mess with any other properties than pos and data
+        new_data = torch_geometric.utils.from_trimesh(trimesh)
+        data.pos = new_data.pos
+        data.face = new_data.face
+        return data
